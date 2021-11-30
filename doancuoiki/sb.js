04e067cee8b72cbe8eb8659c820dad4b7004aa7d
congminhtran1802 <95283996+congminhtran1802@users.noreply.github.com>
2021-11-30 17:53:01 +0700

    Delete doancuoiki directory

--- doancuoiki/sb.js
+++
@@ -1,8 +0,0 @@
-var sideBar = document.querySelector('.sidebar')
-function sb() {
-    sideBar.classList.toggle('toggle')
-}
-var sideBar1 = document.querySelector('.sidebar-2')
-function sb1() {
-    sideBar1.classList.toggle('toggle')
-}
